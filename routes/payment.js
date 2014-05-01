134ed4de655cfea799ff70f25396e54180cfeaef
Sumit Arora <er.sumitarora@gmail.com>
2014-05-01 12:04:52 -0400

    basic payment working

--- routes/payment.js
+++ routes/payment.js
@@ -1,9 +1,27 @@
+var stripeApiKeyTesting = "sk_test_BQokikJOvBiI2HlWgH4olfQ2";
 var express = require('express');
+var stripe = require('stripe') (stripeApiKeyTesting);
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.post('/', function(req, res) {
+  var stripeToken = req.body.stripeToken;
+
+  stripe.setApiKey("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+  var charge = stripe.charges.create({
+    amount: 1000, // amount in cents, again
+    currency: "usd",
+    card: stripeToken + 'sada',
+    description: "payinguser@example.com"
+  }, function(err, charge) {
+    console.log(err);
+    console.log(charge);
+    if (err && err.type === 'StripeCardError') {
+      
+    }
+  });
+
+  res.send('respond with a resource' + stripeToken);
 });
 
 module.exports = router;
